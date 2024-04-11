541ce8b5e92149e94321803df24073cebb687978
pthulen <pfthulen@gmail.com>
2024-04-11 16:31:20 -0700

    search by id

--- src/components/searchbar.tsx
+++ src/components/searchbar.tsx
@@ -1,10 +1,19 @@
 import { SetStateAction, useContext, useState } from "react";
 import { PokemonContext } from "../context/pokemon";
-import { Box, Input, Button } from "@chakra-ui/react";
+import {
+  Box,
+  Input,
+  Button,
+  Flex,
+  RadioGroup,
+  Stack,
+  Radio,
+} from "@chakra-ui/react";
 
 const SearchBar = () => {
   const { pokemonList, setSelectedPokemon } = useContext(PokemonContext);
   const [searchTerm, setSearchTerm] = useState("");
+  const [searchType, setSearchType] = useState("name");
   const handleInputChange = (e: {
     target: { value: SetStateAction<string> };
   }) => {
@@ -12,10 +21,25 @@ const SearchBar = () => {
   };
 
   const handleSearch = () => {
-    const filteredPokemon = pokemonList.filter((pokemon) =>
-      pokemon.name.includes(searchTerm)
-    );
-    setSelectedPokemon(filteredPokemon[0]);
+    if (searchType === "name") {
+      const filteredPokemon = pokemonList.filter((pokemon) =>
+        pokemon.name.includes(searchTerm)
+      );
+      setSelectedPokemon(filteredPokemon[0]);
+    } else {
+      fetch(`https://pokeapi.co/api/v2/pokemon/${searchTerm}`)
+        .then((res) => res.json())
+        .then((data) => {
+          setSelectedPokemon({
+            name: data.name,
+            url: `https://pokeapi.co/api/v2/pokemon/${data.id}`,
+          });
+        });
+    }
+  };
+
+  const handleRadioChange = (value: string) => {
+    setSearchType(value);
   };
 
   return (
@@ -25,9 +49,17 @@ const SearchBar = () => {
         _placeholder={{ color: "white" }}
         onChange={handleInputChange}
       />
-      <Button colorScheme="blackAlpha" mt={2} onClick={handleSearch}>
-        Search
-      </Button>
+      <Flex>
+        <Button colorScheme="blackAlpha" mt={2} onClick={handleSearch}>
+          Search
+        </Button>
+        <RadioGroup onChange={handleRadioChange} value={searchType} m={2}>
+          <Stack direction="row">
+            <Radio value="name">Name</Radio>
+            <Radio value="type">Id</Radio>
+          </Stack>
+        </RadioGroup>
+      </Flex>
     </Box>
   );
 };
